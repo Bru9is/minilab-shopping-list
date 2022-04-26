b18d7d2b34c8cb0e46c235b4f3a9c33d540d15b4
Bruno Novis <bru9is@outlook.com>
2022-04-25 21:57:28 -0300

    done

---
+++ index.js
@@ -0,0 +1,39 @@
+
+const newItemInput = document.getElementById('new-item')
+const newItemBtn = document.getElementById('btn-add-item')
+
+function addNewItem() {
+    if(newItemInput.value) {
+        const li = document.createElement('li')
+        li.setAttribute('class', 'list-group-item d-flex justify-content-between')
+        const liContent = `
+        ${newItemInput.value} <button class="btn btn-remove btn-danger">-</button>
+        `
+        li.innerHTML = liContent
+        const ul = document.querySelector('ul')
+        ul.appendChild(li)
+
+        newItemInput.value = ''
+
+        const button = li.querySelector('button')
+        button.addEventListener('click', removeItem)
+    }
+}
+
+function removeItem(event){
+    const button = event.currentTarget
+    const li = button.parentNode
+    const ul = button.parentNode.parentNode
+    ul.removeChild(li)
+}
+
+console.log('teste')
+window.addEventListener('load', () => {
+    const btn = document.getElementById('btn-add-item')
+    btn.addEventListener('click', addNewItem)
+
+    const btnsRemove = document.getElementsByClassName('btn-remove')
+    for (let btnRemove of btnsRemove){
+        btnRemove.addEventListener('click', removeItem)
+    }
+})
